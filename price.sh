531c6f51b47c089f7216f9d42e860c6321c0d427
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2020-08-03 14:04:31 +0900

    Calculate Price (based on age and membership)

---
+++ price.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+read -p "年齢を入力してください。" age
+if [ $age -le 15 ]; then
+    echo 料金は800円です。
+else
+    read -p "会員ですか？ [y/n]" yn
+    if [ $yn = "y" ]; then
+    echo 料金は1000円です。
+    else
+    echo 料金は1800円です。
+    fi
+fi
